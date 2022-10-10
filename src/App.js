2b132dbe82a4029d68a88fec6d5f372b04291ea9
Gerald Ezenagu <egerald344@gmail.com>
2022-10-10 21:35:39 +0100

    working on tip

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h2>SPLI<br/>TTER</h2>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
