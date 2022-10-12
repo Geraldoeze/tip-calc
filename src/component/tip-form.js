54989bc9165af361160bae13ce23b62adc835c69
Gerald Ezenagu <egerald344@gmail.com>
2022-10-12 22:41:40 +0100

    form issues

--- src/component/tip-form.js
+++ src/component/tip-form.js
@@ -2,27 +2,38 @@ import React from "react";
 import './tipStyles.css';
 
 const TipCal = () => {
-    const percentage = [5, 10, 15, 25, 50]
+    const percentage = [5, 10, 15, 25, 50];
+    const SubmitHandler = (event) => {
+
+    }
     return ( 
         <div className="tip-container ">
             <div className="tip-form">
                 <form>
                     <label htmlFor="Bill">Bill</label> 
-                    <input className="tip-input"  type="text"/>
+                    <input className="tip-input"  type="number"/>
                     <label htmlFor="tip">Select Tip %</label>
                     {percentage.map((per, id, arr) => {
                         return (
                         <button key={id}>{per}%</button>
                         )
                     })}
-                        <input />
+                        <input className="tip-input__odd" type="number"/>
                     <label htmlFor="number" >Number of People</label>
-                    <input className="tip-input" type="text" />
+                    <input className="tip-input" type="number" />
                 
                 </form>
             </div>
             <div className="tip-result">
-
+              <div className="tip-amount">
+                <h3>Tip Amount<br /><span>/ person</span></h3>
+                <p>$0.00</p>
+              </div>
+              <div className="tip-amount">
+                <h3>Total<br /><span>/ person</span></h3>
+                <p>$0.00</p>
+              </div>
+                <button type='reset' onClick={SubmitHandler}>RESET</button>
             </div>
         </div>
      );
