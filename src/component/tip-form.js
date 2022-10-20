11caa5bd330643b9574779e603d16f260063f8a5
Gerald Ezenagu <egerald344@gmail.com>
2022-10-20 21:34:50 +0100

    added icons

--- src/component/tip-form.js
+++ src/component/tip-form.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import './tipStyles.css';
+import {FaDollarSign} from 'react-icons/fa';
+import {BsPersonFill} from 'react-icons/bs';
 
 const TipCal = () => {
     const percentage = [5, 10, 15, 25, 50];
-    console.log(document.getElementById("bill"))
+    // const [query, SetQuery] = useState()
+
+
+
     const SubmitHandler = () => {
         // event.preventDefault();
         console.log()
     }
+    const changeHandler = (e) => {
+        return (
+            console.log(e.target.value)   
+        )
+    } 
+    console.log()
     return ( 
         <form className="tip-container " onSubmit={SubmitHandler}> 
             <div className="tip-form">   
 
-                <label htmlFor="Bill">Bill</label> 
-                <input className="tip-input" name="bill" id="bill" type="number" placeholder="0"/>
+                <label htmlFor="Bill">Bill</label>
+                <div className="input-cover">
+                    <span><FaDollarSign/></span> 
+                    <input 
+                        className="tip-input" 
+                        name="bill" 
+                        id="bill" 
+                        type="number" 
+                        placeholder="0"
+                        onChange={changeHandler}
+                    />
+                </div>
+                
                 <label htmlFor="tip">Select Tip %</label>
                 <div className="btn_cover">
                 {percentage.map((per, id, arr) => {
@@ -21,10 +43,28 @@ const TipCal = () => {
                         <button className="btn_keys" type="button" key={id}>{per}%</button>
                     )
                 })}
-                <input className="tip-input__odd" name="customTip" id="customTip" type="number" placeholder="Custom"/>
+                <input 
+                   className="tip-input__odd" 
+                   name="customTip" 
+                   id="customTip" 
+                   type="number" 
+                   placeholder="Custom"
+                   onChange={changeHandler}
+                />
                 </div>                
                 <label htmlFor="number" >Number of People</label>
-                <input className="tip-input" name="people" id="people" type="number" placeholder="0"/>    
+                <div className="input-cover">
+                    <span><BsPersonFill/></span>
+                    <input 
+                        className="tip-input" 
+                        name="people" 
+                        id="people" 
+                        type="number" 
+                        placeholder="0"
+                        onChange={changeHandler}
+                    />
+                </div>
+                    
 
             </div>
             <div className="tip-result">
